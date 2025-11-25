ac68d634abc6e739b7fd54d0ac4a5c49afd515ff
たいくん <98800458+taikun114@users.noreply.github.com>
2025-11-25 22:04:12 +0900

    Improve the integration to reload when the entry name is changed

--- custom_components/voicevox_tts/__init__.py
+++ custom_components/voicevox_tts/__init__.py
@@ -23,6 +23,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             async with session.get(url, timeout=5) as response:
                 response.raise_for_status()
                 await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+                entry.async_on_unload(entry.add_update_listener(async_reload_entry))
                 return True
     except (aiohttp.ClientError, asyncio.TimeoutError) as e:
         raise ConfigEntryNotReady(f"Failed to connect to VOICEVOX Engine: {e}")
@@ -33,3 +34,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+
+async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Reload the config entry when it is updated."""
+    await hass.config_entries.async_reload(entry.entry_id)
